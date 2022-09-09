49dfca1e7627df0f2d544286ba45c8edc7cd4623
Sven Staffl <stafflsv@gmail.com>
2022-09-09 11:54:32 +0200

    Add additional text to storybook

--- stories/Mindmapr.stories.tsx
+++ stories/Mindmapr.stories.tsx
@@ -21,7 +21,15 @@ const meta: Meta = {
 
 export default meta;
 
-const Template = <T extends HasIdAndChildren>(args: MindmaprProps<T>) => {
+interface DokuProps {
+  '#notPartOfLibrary_tell_to_color_background': boolean;
+  '#notPartOfLibrary_showSelectionDoku': boolean;
+  '#notPartOfLibrary_showEnterTabDoku': boolean;
+}
+
+const Template = <T extends HasIdAndChildren>(
+  args: MindmaprProps<T> & DokuProps
+) => {
   const [state, setState] = React.useState<T | undefined>();
   const inputStateRef = React.useRef<T>();
   React.useEffect(() => {
@@ -35,9 +43,48 @@ const Template = <T extends HasIdAndChildren>(args: MindmaprProps<T>) => {
     return <div></div>;
   }
   return (
-    <div style={{ width: 600, height: 300, margin: 'auto' }}>
-      <Mindmapr {...newArgs} items={state} />
-    </div>
+    <>
+      <div style={{ width: 600, height: 300, margin: 'auto' }}>
+        <Mindmapr {...newArgs} items={state} />
+      </div>
+
+      <div
+        style={{
+          position: 'absolute',
+          top: 20,
+          right: 20,
+          width: 200,
+          borderLeft: '1px solid #ddd',
+          borderBottom: '1px solid #ddd',
+          borderRadius: 4,
+        }}
+      >
+        <ul>
+          {!args['#notPartOfLibrary_tell_to_color_background'] ? null : (
+            <li>
+              The lines extend from the center of the parent to the beginning of
+              the child element. You need to add a background-color to your
+              rendered elements to cover the lines
+            </li>
+          )}
+          {!args['#notPartOfLibrary_showSelectionDoku']
+            ? null
+            : [
+                <li key="1">You can select an item by clicking</li>,
+                <li key="2">You can move selection with the arrow keys</li>,
+              ]}
+
+          {!args['#notPartOfLibrary_showEnterTabDoku']
+            ? null
+            : [
+                <li key="3">"ENTER" adds a new element on same depth level</li>,
+                <li key="4">
+                  "TAB" adds a new element one depth level deeper on child level
+                </li>,
+              ]}
+        </ul>
+      </div>
+    </>
   );
 };
 
@@ -147,6 +194,7 @@ export const ReadonlyDefault = Template.bind({});
 ReadonlyDefault.args = {
   renderItem,
   items,
+  '#notPartOfLibrary_tell_to_color_background': true,
 };
 
 const renderItemNice = (
@@ -161,6 +209,7 @@ export const ReadonlyWithStyledItems = Template.bind({});
 ReadonlyWithStyledItems.args = {
   renderItem: renderItemNice,
   items,
+  '#notPartOfLibrary_showSelectionDoku': true,
 };
 
 export const AddAndDeleteWithStyledItems = Template.bind({});
@@ -173,6 +222,8 @@ AddAndDeleteWithStyledItems.args = {
     children: [],
     name: 'new child',
   }),
+  '#notPartOfLibrary_showSelectionDoku': true,
+  '#notPartOfLibrary_showEnterTabDoku': true,
 };
 
 //todo lines don't get rerendered properly in storybook, when side changes back to "both"
